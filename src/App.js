dc07a820a5fc0a530157818660b32aee323e25ae
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-01-16 02:28:03 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-
+import Footer from "./components/Footer";
 const App = () => {
   return (
+    <div>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
+        <Footer/>
+        </div>
   );
 };
 
