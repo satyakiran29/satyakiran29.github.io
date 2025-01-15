9c1aa8cc5aca8f662ea8694a927344ce8dc7bef6
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-01-16 02:56:17 +0530

    Loaders Added
    Hashloader From https://www.davidhu.io/react-spinners/

--- src/App.js
+++ src/App.js
@@ -3,18 +3,54 @@ import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Footer from "./components/Footer";
-const App = () => {
+
+
+import { useEffect, useState } from "react";
+
+
+import HashLoader from "react-spinners/HashLoader";
+
+
+function App() {
+  const[Loading,SetLoading]=useState(true);
+
+  useEffect(()=>{
+    SetLoading(true)
+
+    setTimeout(()=>{
+    SetLoading(false)}
+    ,1900)
+  },[])  
+  
   return (
-    <div>
-    <Router>
+    <>
+      {Loading ? (
+      <div className="loader"> 
+        <HashLoader
+          color={'#38761d'}
+          loading={true}
+          size={100}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
+      ):(
+      <div>
+      
+
+
+<Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>}/>
+
       </Routes>
-    </Router>
-        <Footer/>
-        </div>
+      </Router>
+      <Footer/>
+      </div>
+      )}  
+    </>
   );
-};
+}
 
 export default App;
