98d273df6c37a0303ccd24d91026e2368fdebde9
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-02-05 07:25:36 +0530

    Convert jpg to webp to improve the images loading speed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -19,27 +19,28 @@ import lgShare from 'lightgallery/plugins/share';
 import lgRotate from 'lightgallery/plugins/rotate';
 
 const images = [
-    { src: "/gallery/1.jpg", alt: "1" }, 
-  { src: "https://server.skdev29.workers.dev/1:/3.jpg", alt: "3" },
-  { src: "https://server.skdev29.workers.dev/1:/4.jpg", alt: "4" },
-  { src: "https://server.skdev29.workers.dev/1:/5.jpg", alt: "5" },
-  { src: "https://server.skdev29.workers.dev/1:/6.jpg", alt: "6" },
-  { src: "https://server.skdev29.workers.dev/1:/7.jpg", alt: "7" },
-  { src: "https://server.skdev29.workers.dev/1:/8.jpg", alt: "8" },
-  { src: "https://server.skdev29.workers.dev/1:/9.jpg", alt: "9" },
-  { src: "https://server.skdev29.workers.dev/1:/10.jpg", alt: "10" },
-  { src: "https://server.skdev29.workers.dev/1:/11.jpg", alt: "11" },
-  { src: "https://server.skdev29.workers.dev/1:/12.jpg", alt: "12" },
-  { src: "https://server.skdev29.workers.dev/1:/13.jpg", alt: "13" },
-  { src: "https://server.skdev29.workers.dev/1:/14.jpg", alt: "14" },
-  { src: "https://server.skdev29.workers.dev/1:/15.jpg", alt: "15" },
-  { src: "https://server.skdev29.workers.dev/1:/16.jpg", alt: "16" },
-  { src: "https://server.skdev29.workers.dev/1:/17.jpg", alt: "17" },
-  { src: "https://server.skdev29.workers.dev/1:/18.jpg", alt: "18" },
-  { src: "https://server.skdev29.workers.dev/1:/19.jpg", alt: "19" },
-  { src: "https://server.skdev29.workers.dev/1:/20.jpg", alt: "20" },
-  { src: "https://server.skdev29.workers.dev/1:/21.jpg", alt: "21" },
-  { src: "https://server.skdev29.workers.dev/1:/22.jpg", alt: "22" }
+    
+  { "src": "https://server.skdev29.workers.dev/1:/3.webp", "alt": "3" },
+  { "src": "https://server.skdev29.workers.dev/1:/4.webp", "alt": "4" },
+  { "src": "https://server.skdev29.workers.dev/1:/5.webp", "alt": "5" },
+  { "src": "https://server.skdev29.workers.dev/1:/6.webp", "alt": "6" },
+  { "src": "https://server.skdev29.workers.dev/1:/7.webp", "alt": "7" },
+  { "src": "https://server.skdev29.workers.dev/1:/8.webp", "alt": "8" },
+  { "src": "https://server.skdev29.workers.dev/1:/9.webp", "alt": "9" },
+  { "src": "https://server.skdev29.workers.dev/1:/10.webp", "alt": "10" },
+  { "src": "https://server.skdev29.workers.dev/1:/11.webp", "alt": "11" },
+  { "src": "https://server.skdev29.workers.dev/1:/12.webp", "alt": "12" },
+  { "src": "https://server.skdev29.workers.dev/1:/13.webp", "alt": "13" },
+  { "src": "https://server.skdev29.workers.dev/1:/14.webp", "alt": "14" },
+  { "src": "https://server.skdev29.workers.dev/1:/15.webp", "alt": "15" },
+  { "src": "https://server.skdev29.workers.dev/1:/16.webp", "alt": "16" },
+  { "src": "https://server.skdev29.workers.dev/1:/17.webp", "alt": "17" },
+  { "src": "https://server.skdev29.workers.dev/1:/18.webp", "alt": "18" },
+  { "src": "https://server.skdev29.workers.dev/1:/19.webp", "alt": "19" },
+  { "src": "https://server.skdev29.workers.dev/1:/20.webp", "alt": "20" },
+  { "src": "https://server.skdev29.workers.dev/1:/21.webp", "alt": "21" },
+  { "src": "https://server.skdev29.workers.dev/1:/22.webp", "alt": "22" },
+    { "src": "https://server.skdev29.workers.dev/1:/23.webp", "alt": "23" }
 
 ]
 
