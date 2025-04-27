84048c4c0919d9d9350579bcae8e88c6e71c32fe
Satyakiran <87798342+satyakiran29@users.noreply.github.com>
2025-04-27 12:37:50 +0530

    Update Home.js

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../home/home.css"; // Assuming your CSS file is in the same directory
 import { FaGithub, FaYoutube, FaInstagram } from "react-icons/fa"; // Use the icons from react-icons
 import p1 from  "../../src/nav/imgs/2.gif";
+import { Analytics } from '@vercel/analytics/react';
 const Home = () => {
   return (
     <div>
+      <Analytics />
       {/* Main Section */}
       <section className="main">
         <img src={p1} alt="Profile" />
